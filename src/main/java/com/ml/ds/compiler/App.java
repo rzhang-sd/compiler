3affdef52cefbabd8a523e998cdab13e488bfaa5
Rui Zhang <ruizhang@Ruis-MBP.lan1>
2020-01-20 23:47:11 -0800

    format

--- src/main/java/com/ml/ds/compiler/App.java
+++ src/main/java/com/ml/ds/compiler/App.java
@@ -6,7 +6,7 @@ package com.ml.ds.compiler;
  */
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
